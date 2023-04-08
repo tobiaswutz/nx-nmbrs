4cde4f92a28da4c844e091817e47eb2b5e22d52b
Tobias Wutz <tobiaswutz22@gmail.com>
2023-04-08 12:30:34 +0200

    trigger

--- apps/server/src/user/user.service.ts
+++ apps/server/src/user/user.service.ts
@@ -4,17 +4,16 @@ import { EditUserDto } from './dto';
 
 @Injectable()
 export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
 
-    constructor(private readonly prisma: PrismaService) { }
-
-    async editUser(userId: number, dto: EditUserDto) {
-        const user = await this.prisma.user.update({
-            where: { id: userId },
-            data: {
-                ...dto,
-            },
-        });
-        delete user.hash;
-        return user;
-    }
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: { id: userId },
+      data: {
+        ...dto,
+      },
+    });
+    delete user.hash;
+    return user;
+  }
 }
